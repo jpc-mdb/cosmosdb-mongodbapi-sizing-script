c27fa66807f05b9f3f927ad6078cefdc69e385ae
Jean-Philippe Chenot <jeanphilippe.chenot@Jean-Philippes-MacBook-Pro-2.local>
2021-10-18 15:53:54 +0100

    Added the missing script file

---
+++ cosmos-sizing-script.ps1
@@ -0,0 +1,48 @@
+
+# Get the list of all document DBs in the Azure account
+$ids = (Get-AzResource -ResourceType Microsoft.DocumentDB/databaseAccounts).ResourceId
+
+# Get data points for each DB
+foreach($item in $ids){
+    # $resource = (Get-AzResource -ResourceId $item)
+    # $account = Get-AzCosmosDBAccount -ResourceGroupName "mdb_sa" -Name "boots"
+
+    # Get data usage in GB
+    $metric = Get-AzMetric -ResourceId $item -MetricName "DataUsage" -WarningAction Ignore
+    $data = ($metric.Data | Select-Object -Last 1).Total/1024/1024/1024
+    Write-Output "$name total Data Usage: $data GB"
+
+    # Get index usage in GB
+    $metric = Get-AzMetric -ResourceId $item -MetricName "IndexUsage" -WarningAction Ignore
+    $data = ($metric.Data | Select-Object -Last 1).Total/1024/1024/1024
+    Write-Output "$name total Index Usage: $data GB"
+
+    # Get number of requests
+    $startTime = (Get-Date).AddDays(-90).ToString("yyyy-MM-ddTHH:mm:ssZ")
+    $metric = Get-AzMetric -ResourceId $item -MetricName "MongoRequests" -WarningAction Ignore -TimeGrain 12:00:00 -StartTime $startTime
+    $data = 0
+    foreach($d in $metric.Data){
+        $data += $d.Count
+    }
+    $firstRequest = ($metric.Data | Select-Object -First 1).TimeStamp
+    Write-Output "$name total Requests: $data since $firstRequest"
+
+    # Get collections and indexes
+    $accountName = (Get-AzResource -ResourceId $item).Name
+    $mongodbDatabase = (Get-AzCosmosDBMongoDBDatabase -ResourceGroupName "mdb_sa" -AccountName $accountName)
+    $mongodbCollections = (Get-AzCosmosDBMongoDBCollection -ResourceGroupName "mdb_sa" -AccountName $accountName -DatabaseName $mongodbDatabase.Name)
+    $totalIndexes = 0
+    $totalCollections = $mongodbCollections.Count
+    
+    foreach($coll in $mongodbCollections){
+        $nIndexes = $coll.Resource.Indexes.Count
+        $totalIndexes += $nIndexes
+    }
+    Write-Output "$accountName total Collections: $totalCollections"
+    Write-Output "$accountName total Indexes: $totalIndexes"
+
+    # Get documents
+    $metric = Get-AzMetric -ResourceId $item -MetricName "DocumentCount" -WarningAction Ignore
+    $data = ($metric.Data | Select-Object -Last 1).Total
+    Write-Output "$accountName total Documents: $data"
+}
