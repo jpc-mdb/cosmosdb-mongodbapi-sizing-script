2d7cd6d0faea3e780cfbb39948f99b11b9012b7d
Jean-Philippe Chenot <jeanphilippe.chenot@Jean-Philippes-MacBook-Pro-2.local>
2021-11-15 11:51:13 +0000

    Improved the code to reduce duplication of data and improve code reusability

--- cosmos-sizing-script-json.ps1
+++ cosmos-sizing-script-json.ps1
@@ -1,3 +1,27 @@
+#Global function to insert documents in a MongoDB Atlas cluster
+function Save-Document {
+    
+    param (
+        $Document,
+        $Collection
+    )
+
+    # Push the data to MongoDB Atlas
+    Import-Module Mdbc
+
+    # Connect to MongoDB and insert the document
+    $db = '[DB NAME]'
+    $connString = 'mongodb+srv://[USERNAME:PASSWORD]@[URL]/?[OPTIONS]'
+
+    try {
+            Connect-Mdbc -ConnectionString $connString -DatabaseName $db -CollectionName $Collection
+            Add-MdbcData -InputObject $Document 
+        }
+    catch {
+            Write-Host "An error occurred:"
+            Write-Host $_
+        }
+}
 
 Write-Output "start executing sizing script..."
 # Get the list of all document DBs in the Azure account
@@ -11,24 +35,93 @@ foreach($item in $ids){
     $mongodbDatabases = (Get-AzCosmosDBMongoDBDatabase -ResourceGroupName $resourceGroupName -AccountName $accountName)
     $startTime = (Get-Date).AddDays(-30).ToString("yyyy-MM-ddTHH:mm:ssZ") # last 30 days
 
-    foreach($db in $mongodbDatabases){
-        $dbName = $db.Name
+    # Get data usage in GB
+    $metric = Get-AzMetric -ResourceId $item -MetricName "DataUsage" -WarningAction Ignore
+    $dataUsage = ($metric.Data | Select-Object -Last 1).Total/1024/1024/1024
 
-        # Get data usage in GB
-        $metric = Get-AzMetric -ResourceId $item -MetricName "DataUsage" -WarningAction Ignore
-        $dataUsage = ($metric.Data | Select-Object -Last 1).Total/1024/1024/1024
+    # Get index usage in GB
+    $metric = Get-AzMetric -ResourceId $item -MetricName "IndexUsage" -WarningAction Ignore
+    $indexUsage = ($metric.Data | Select-Object -Last 1).Total/1024/1024/1024
 
-        # Get index usage in GB
-        $metric = Get-AzMetric -ResourceId $item -MetricName "IndexUsage" -WarningAction Ignore
-        $indexUsage = ($metric.Data | Select-Object -Last 1).Total/1024/1024/1024
+    # Get number of requests in the last 30 days
+    $metric = Get-AzMetric -ResourceId $item -MetricName "MongoRequests" -WarningAction Ignore -TimeGrain 12:00:00 -StartTime $startTime
+    $requests = 0
+    foreach($d in $metric.Data){
+        $requests += $d.Count
+    }
+    $firstRequest = ($metric.Data | Select-Object -First 1).TimeStamp
 
-        # Get number of requests in the last 30 days
-        $metric = Get-AzMetric -ResourceId $item -MetricName "MongoRequests" -WarningAction Ignore -TimeGrain 12:00:00 -StartTime $startTime
-        $requests = 0
-        foreach($d in $metric.Data){
-            $requests += $d.Count
+    # Get number of request units in the last 30 days
+    $metric = Get-AzMetric -ResourceId $item -MetricName "TotalRequestUnits" -WarningAction Ignore -TimeGrain 00:01:00 -StartTime $startTime
+    $requestUnits = 0
+    foreach($d in $metric.Data){
+        $requestUnits += $d.Total
+    }
+
+    $averageRequestsPerMinute = 0
+    if($metric.Data.Count -gt 0) {
+        $averageRequestsPerMinute = $requestUnits / $metric.Data.Count
+    }
+
+    $maxRequestUnits = 0
+    foreach($d in $metric.Data){
+        if($d.Total -gt $maxRequestUnits){
+            $maxRequestUnits = $d.Total
         }
-        $firstRequest = ($metric.Data | Select-Object -First 1).TimeStamp
+    }
+
+    # Get number of Mongo request charges in the last 30 days
+    $metric = Get-AzMetric -ResourceId $item -MetricName "MongoRequestCharge" -WarningAction Ignore -TimeGrain 00:01:00 -StartTime $startTime
+    $requestCharge = 0
+    foreach($d in $metric.Data){
+        $requestCharge += $d.Total
+    }
+
+    $averageChargesPerMinute = 0
+    if($metric.Data.Count -gt 0) {
+        $averageChargesPerMinute = $requestCharge / $metric.Data.Count
+    }
+
+    $maxRequestCharges = 0
+    foreach($d in $metric.Data){
+        if($d.Total -gt $maxRequestCharges){
+            $maxRequestCharges = $d.Total
+        }
+    }
+
+    # Get documents
+    $metric = Get-AzMetric -ResourceId $item -MetricName "DocumentCount" -WarningAction Ignore
+    $docs = ($metric.Data | Select-Object -Last 1).Total
+
+    # Create the document to insert
+    $doc = [PSCustomObject]@{
+        ts = Get-Date
+        account_name = $accountName
+        resource_group_name = $resourceGroupName
+        data_usage_gb = $dataUsage
+        index_usage_db = $indexUsage
+        total_requests = $requests
+        requests_start = $firstRequest
+        request_units_30_days = @{
+            total = $requestUnits
+            average_per_minute = $averageRequestsPerMinute
+            max_per_minute = $maxRequestUnits
+        }
+        mongo_request_charges_30_days = @{
+            total = $requestCharge
+            average_per_minute = $averageChargesPerMinute
+            max_per_minute = $maxRequestChargesclear
+        }
+        total_documents = $docs
+    }
+
+    # Insert the document
+    $coll = $accountName.Trim() + '-' + $resourceGroupName.Trim()
+    Save-Document -Document $doc -Collection $coll
+
+    # Repeat the process for DB specific collection data
+    foreach($db in $mongodbDatabases){
+        $dbName = $db.Name       
 
         # Get collections and indexes
         $mongodbCollections = (Get-AzCosmosDBMongoDBCollection -ResourceGroupName $resourceGroupName -AccountName $accountName -DatabaseName $dbName)
@@ -40,88 +133,22 @@ foreach($item in $ids){
             $totalIndexes += $nIndexes
             # $throughput = Get-AzCosmosDBMongoDBCollectionThroughput -ResourceGroupName $resourceGroupName -AccountName $accountName -DatabaseName $dbName -Name $coll.Name | Select-Object {$resourceGroupName}, {$caccountName}, {$dbName}, {$coll.Name}, Throughput | Format-Table
             # Write-Ouput($coll.Name + ": " + $throughput)
-        }
-
-        # Get documents
-        $metric = Get-AzMetric -ResourceId $item -MetricName "DocumentCount" -WarningAction Ignore
-        $docs = ($metric.Data | Select-Object -Last 1).Total
-
-        # Get number of request units in the last 30 days
-        $metric = Get-AzMetric -ResourceId $item -MetricName "TotalRequestUnits" -WarningAction Ignore -TimeGrain 00:01:00 -StartTime $startTime
-        $requestUnits = 0
-        foreach($d in $metric.Data){
-            $requestUnits += $d.Total
-        }
-
-        $averageRequestsPerMinute = 0
-        if($metric.Data.Count -gt 0) {
-            $averageRequestsPerMinute = $requestUnits / $metric.Data.Count
-        }
-
-        $maxRequestUnits = 0
-        foreach($d in $metric.Data){
-            if($d.Total -gt $maxRequestUnits){
-                $maxRequestUnits = $d.Total
-            }
-        }
-
-        # Get number of Mongo request charges in the last 30 days
-        $metric = Get-AzMetric -ResourceId $item -MetricName "MongoRequestCharge" -WarningAction Ignore -TimeGrain 00:01:00 -StartTime $startTime
-        $requestCharge = 0
-        foreach($d in $metric.Data){
-            $requestCharge += $d.Total
-        }
-
-        $averageChargesPerMinute = 0
-        if($metric.Data.Count -gt 0) {
-            $averageChargesPerMinute = $requestCharge / $metric.Data.Count
-        }
-
-        $maxRequestCharges = 0
-        foreach($d in $metric.Data){
-            if($d.Total -gt $maxRequestCharges){
-                $maxRequestCharges = $d.Total
-            }
-        }
-
-        # Push the data to MongoDB Atlas
-        Import-Module Mdbc
-
-        # Connect to MongoDB and insert the document
-        $db = '[DB NAME]'
-        $coll = $accountName.Trim() + '-' + $dbName.Trim()
-        $connString = 'mongodb+srv://[USERNAME:PASSWORD]@[URL]/?[OPTIONS]'
+        }     
         
-        Connect-Mdbc -ConnectionString $connString -DatabaseName $db -CollectionName $coll
-        
-        $document = [PSCustomObject]@{
+        # Create the document to insert
+        $collectionData = [PSCustomObject]@{
+            ts = Get-Date
             account_name = $accountName
             resource_group_name = $resourceGroupName
             database_name = $dbName
-            data_usage_gb = $dataUsage
-            index_usage_db = $indexUsage
-            total_requests = $requests
-            requests_start = $firstRequest
-            ts = Get-Date
-            collections = @{
-                total_collections = $totalCollections
-                total_indexes = $totalIndexes
-                total_documents = $docs
-            }
-            request_units_30_days = @{
-                total = $requestUnits
-                average_per_minute = $averageRequestsPerMinute
-                max_per_minute = $maxRequestUnits
-            }
-            mongo_request_charges_30_days = @{
-                total = $requestCharge
-                average_per_minute = $averageChargesPerMinute
-                max_per_minute = $maxRequestCharges
-            }
+            total_collections = $totalCollections
+            total_indexes = $totalIndexes            
         }
 
-        Add-MdbcData -InputObject $document
-    }
+        # Insert the document
+        $coll = $accountName.Trim() + '-' + $resourceGroupName.Trim() + '-' + $dbName.Trim()
+        Save-Document -Document $collectionData -Collection $coll
+    }     
 }
 
 Write-Output "...completed executing sizing script"
